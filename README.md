33e95e01c71f8a4d5a0217c9bd22973776a0ca14
Ratchapol Gunhompoo <144249355+sicsechub@users.noreply.github.com>
2026-03-05 17:31:14 +0700

    DEV A feat: strengthen login auth   (#1)
    * feat: strengthen login auth
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Makey1153 <wanchaloam.p@kkumail.com>

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+
+# README.md
 # Cyber Security Project
 Dev Name: [Wanchaloam Pugdee] [663380133-4]
+Dev Name: [Ratchapol] [663380126-1]
+
+
--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
+# src/app.py
 def login(username, password):
-    """Basic login function"""
-    if username == "admin" and \
-       password == "secure_pw":
-        return True
+    if username == "admin" and password == "secure_pw": # เปลี่ยน
+        return "Login Success"
+    return "Login Failed"
