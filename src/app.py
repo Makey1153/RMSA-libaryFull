d5c5e3565259cd1612055202a263884d753fed89
tin-joe <ginhanataki@gmail.com>
2026-03-05 17:37:35 +0700

    Feature logging (#3)
    * feat: add logging
    
    * feat: add login attempt log
    
    ---------
    
    Co-authored-by: Makey1153 <wanchaloam.p@kkumail.com>

--- src/app.py
+++ src/app.py
@@ -1,8 +1,8 @@
 # src/app.py
 def login(username, password):
     """Basic login function"""
+    print("Login attempt detected")
     print("yooho")
     if username == "admin" and password == "secure_pw":
         return "Login Success"
     return "Login Failed"
-
